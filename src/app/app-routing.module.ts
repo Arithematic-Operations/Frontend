26d24ec339b25d1b51b384cd6e69aca7b43ca8e2
Shruti2906 <65824891+Shruti2906@users.noreply.github.com>
2022-12-06 18:10:01 -0800

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdditionComponent } from './pages/addition/addition.component';
+import { MultiplicationComponent } from './pages/multiplication/multiplication.component';
+import { SubstractionComponent } from './pages/substraction/substraction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"add/",
+    component:AdditionComponent,
+    pathMatch:'full'
+  },
+  {
+    path:"sub/",
+    component:SubstractionComponent,
+    pathMatch:'full'
+  }
+  ,{
+    path:"mul/",
+    component:MultiplicationComponent,
+    pathMatch:'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
